d776ced66e8ad08c643703002a3e6295876e25f3
Marcin Kalinoski <kalinowskixmarcin@o2.pl>
2017-08-22 19:02:29 +0200

    sfdasfafasfasdfsd

--- index.php
+++ index.php
@@ -3,12 +3,14 @@ $urlClean = $_SERVER['HTTP_HOST'].htmlspecialchars($_SERVER['REQUEST_URI']);
 $urlFull = $_SERVER['HTTP_HOST'].substr($_SERVER['REQUEST_URI'], 0, strrpos($_SERVER['REQUEST_URI'], '/') + 1);
 ?>
 <!-- http://sigmeo.pl/themeforest/_blustein/ -->
+<!-- http://nataliakielbicka.pl/skills -->
 <!DOCTYPE html>
 <html>
   <head>
     <meta charset="utf-8">
     <title>Portfolio - Marcin Kalinowski</title>
     <link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,500,600,700,800,900&amp;subset=latin-ext" rel="stylesheet">
+    <link rel="stylesheet" href="dist/css/reset.css">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <?php include('src/php/open_graph.php'); ?>
   </head>
